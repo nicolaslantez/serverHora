82170fc18d5106fbafa1b141553dbef45ac5f5d4
nicolaslantez <nicolas.lantez96@gmail.com>
2016-12-01 15:53:52 -0300

    index x default

--- ejServer.js
+++ ejServer.js
@@ -5,6 +5,18 @@ var server = http.createServer();
 
 server.on("request",function(req,res){
 	var urlData = url.parse(req.url,true);
+	if(urlData.pathname = "/"){
+		fs.readFile("public/index.html",function(err,html){
+			if(err){
+				res.writeHead(500);
+				res.end("Ha ocurrido algo malo");
+			}
+			else{
+			res.end(html)
+			}	
+		})
+	}
+	else{
 	var pathname = "public"+ urlData.pathname;
 	fs.exists(pathname,function(exists){
 		if(exists){
@@ -21,6 +33,7 @@ server.on("request",function(req,res){
 			res.end("No existe!");
 		}
 	});
+	}
 });
 
 server.listen(process.env.PORT || 3000);
