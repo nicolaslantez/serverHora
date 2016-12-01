045abd8729a2b36f519f9479ddfa230765bce06a
nicolaslantez <nicolas.lantez96@gmail.com>
2016-12-01 13:56:51 -0300

    Prueba url

--- ejServer.js
+++ ejServer.js
@@ -1,13 +1,18 @@
 var http = require("http");
+var url = require("url");
 var server = http.createServer();
 
 server.on("request",function(req,res){
-	var d = new Date();
-	var hora = d.getHours();
-	var minutos = d.getMinutes();
-	var segundos = d.getSeconds();
+	// var d = new Date();
+	// var hora = d.getHours();
+	// var minutos = d.getMinutes();
+	// var segundos = d.getSeconds();
 
-	var horaActual = hora + ":" + minutos + ":" + segundos;
-	res.end(horaActual);
+	// var horaActual = hora + ":" + minutos + ":" + segundos;
+	// res.end(horaActual);
+
+	var urlData = url.parse(req.url,true);
+	res.end(JSON.stringify(urlData));
 });
+
 server.listen(process.env.PORT || 3000);
