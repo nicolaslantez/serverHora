3fff1d79b83632ac9fd284faa6fd7cdcc3e857bf
nicolaslantez <nicolas.lantez96@gmail.com>
2016-12-02 09:17:32 -0300

    index.html default with /

--- ejServer.js
+++ ejServer.js
@@ -5,19 +5,19 @@ var server = http.createServer();
 
 server.on("request",function(req,res){
 	var urlData = url.parse(req.url,true);
-	if(urlData.pathname = "/"){
-		fs.readFile("public/index.html",function(err,html){
+	var pathname;
+	if(urlData.pathname == "/"){
+		pathname = "public/index.html";
+		fs.readFile(pathname,function(err,html){
 			if(err){
 				res.writeHead(500);
 				res.end("Ha ocurrido algo malo");
-			}
-			else{
-			res.end(html)
+			}else{
+				res.end(html);
 			}	
-		})
-	}
-	else{
-	var pathname = "public"+ urlData.pathname;
+		});
+	}else{
+	pathname = "public"+ urlData.pathname;
 	fs.exists(pathname,function(exists){
 		if(exists){
 			fs.readFile(pathname,function(err,html){
