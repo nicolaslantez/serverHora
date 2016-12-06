60e48ddb843fbd1d03b1d1cc7528187f55de50b8
nicolaslantez <nicolas.lantez96@gmail.com>
2016-12-06 09:59:50 -0300

    leer más

--- public/js/articulo.js
+++ public/js/articulo.js
@@ -9,7 +9,7 @@ function Articulo(title,content,identificador,body){
 
 Articulo.prototype.render = function(){
 	if(samePage == false){
-		return '<article><div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + this.titulo + '</h3></div><div class="panel-body">' + this.contenido + '<p><a href=noticia' + this.id + '.html"> Leer Mas </a></p></div></div></article>';
+		return '<article><div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">' + this.titulo + '</h3></div><div class="panel-body">' + this.contenido + '<p><a href=noticia' + this.id + '.html> Leer Mas </a></p></div></div></article>';
 	}
 	// else{
 	// 	return '<article><div><h3>' + this.titulo + '</h3></div><div><p>' + this.contenido + '</p></div></article><a href="#" id="noticia' + this.id + '-show" class="showLink" onclick="show(noticia' + this.id + ')">Leer más.</a><div id="noticia"'+ this.id + ' class="more"><p>'+ this.cuerpo + '</p></div>';
